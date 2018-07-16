08a81ed29e3c7a196bf346cfe85882f337b55f7b
Erin Green <erin.y.green@gmail.com>
2018-07-16 15:26:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object.assign({}, recipes, {[sugar]:'2 cups'});
   return recipes;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value; 
+  return object;
+}
+
+destructivelyUpdateObjectWithKeyAndValue(recipes, "sugar", "2 cups"); 
