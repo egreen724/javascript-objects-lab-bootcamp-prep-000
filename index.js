a8ef00809a75f64112adc02c8f2fd0c193978869
Erin Green <erin.y.green@gmail.com>
2018-07-16 15:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   return object; 
 }
 
-destructivelyDeleteFromObjectByKey(recipes, "eggs"); 
+destructivelyDeleteFromObjectByKey(recipes, "milk"); 
